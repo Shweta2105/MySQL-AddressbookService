c8c19dc2d1822d539b0ffd8e4b5a2c549c8c04d2
Shweta2105 <sghadi21@gmail.com>
2021-07-13 22:56:44 +0530

    UC1 create database

---
+++ Addressbook_Service.sql
@@ -0,0 +1,23 @@
+/*MySQL Database
+    AddressBook service
+ */
+
+//UC 1
+
+mysql> create database AddressBook_service;
+Query OK, 1 row affected (1.06 sec)
+
+mysql> show databases;
++---------------------+
+| Database            |
++---------------------+
+| addressbook_service |
+| classicmodels       |
+| employee_payroll    |
+| information_schema  |
+| mysql               |
+| payroll_service     |
+| performance_schema  |
+| sys                 |
++---------------------+
+8 rows in set (0.38 sec)
